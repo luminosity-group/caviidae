4d86847855dfc89deb59ae9353768fc959cc33b7
Eric J. Holmes <eric@ejholmes.net>
2012-03-13 12:44:04 -0700

    Alias the original method.

--- lib/caviidae/databasedotcom/client.rb
+++ lib/caviidae/databasedotcom/client.rb
@@ -3,18 +3,9 @@ require 'databasedotcom'
 module Databasedotcom
   class Client
   
-    # Maybe there's a better way to do this but overriding the method should
-    # work
+    alias :original_create :create
     def create(class_or_classname, object_attrs)
-      class_or_classname = find_or_materialize(class_or_classname)
-      json_for_assignment = coerced_json(object_attrs, class_or_classname)
-      result = http_post("/services/data/v#{self.version}/sobjects/#{class_or_classname.sobject_name}", json_for_assignment)
-      new_object = class_or_classname.new
-      JSON.parse(json_for_assignment).each do |property, value|
-        set_value(new_object, property, value, class_or_classname.type_map[property][:type])
-      end
-      id = JSON.parse(result.body)["id"]
-      set_value(new_object, "Id", id, "id")
+      new_object = original_create(class_or_classname, object_attrs)
       store(new_object)
       new_object
     end
